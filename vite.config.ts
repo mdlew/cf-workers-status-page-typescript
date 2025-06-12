3eff658fbae64e1f136612bae7a67b6ae71c4f82
Matthew Lew <mdlew@wustl.edu>
2025-06-12 15:32:33 -0500

    Update package.json
    Remove unplugin-polish-tagged-templates dependency
    Update pnpm-lock.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
-import polishTaggedTemplates from 'unplugin-polish-tagged-templates/vite'
+//import polishTaggedTemplates from 'unplugin-polish-tagged-templates/vite'
 import autoImport from 'unplugin-auto-import/vite'
 
 import type { UserConfig } from 'vite'
@@ -11,9 +11,9 @@ export default {
   plugins: [
     react(),
     vike(),
-    polishTaggedTemplates({
-      clsTags: ['cls'],
-    }),
+    // polishTaggedTemplates({
+    //   clsTags: ['cls'],
+    // }),
     autoImport({
       imports: ['react', 'react-router-dom'],
     }),
