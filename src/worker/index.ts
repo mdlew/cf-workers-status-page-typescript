e3334701b0b1d523cd3f8fe55f066efd3b7ba238
Matthew Lew <mdlew@wustl.edu>
2025-06-15 11:48:20 -0500

    Revert "Update static asset handling"
    This reverts commit f90267ddfec045699eef88b4084e470e77ed7c2a.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,26 +1,34 @@
-import { handleSsr } from "./ssr";
-import { handleFetchError } from "./_helpers";
-import { handleCronTrigger } from "./cron";
-import { handleRemoteMonitors } from "./cron/remote-monitors";
+import { handleSsr } from './ssr'
+import { handleStaticAssets } from './static-assets'
+import { isAssetUrl } from './static-assets/helpers'
+import { handleFetchError } from './_helpers'
+import { handleCronTrigger } from './cron'
+import { handleRemoteMonitors } from './cron/remote-monitors'
 
-import type { FetchHandler } from "#src/types";
+import type { FetchHandler } from '#src/types'
 
 const handleFetchEvent: FetchHandler = async (request, env, context) => {
-  const { url } = request;
-  const userAgent = request.headers.get("User-Agent");
+  const { url } = request
+  const userAgent = request.headers.get('User-Agent')
 
-  const response = await handleSsr(env, url, userAgent);
-  return response || new Response("Not Found", { status: 404 });
-};
+  if (!isAssetUrl(url)) {
+    const response = await handleSsr(env, url, userAgent)
+    if (response !== null) {
+      return response
+    }
+  }
+  const response = await handleStaticAssets(request, env, context)
+  return response
+}
 
 const handler: ExportedHandler<Env> = {
   // Worker startup time limit: 400ms
   // ref: https://developers.cloudflare.com/workers/platform/limits/#worker-startup-time
   fetch: async (request, env, ctx) => {
     try {
-      return await handleFetchEvent(request, env, ctx);
+      return await handleFetchEvent(request, env, ctx)
     } catch (err) {
-      return handleFetchError(err);
+      return handleFetchError(err)
     }
   },
   // Time limit:
@@ -28,17 +36,17 @@ const handler: ExportedHandler<Env> = {
   // When the schedule interval is more than 1 hour, a scheduled Worker may run for up to 15 minutes.
   // ref: https://developers.cloudflare.com/workers/platform/limits/#cpu-time Note block
   scheduled: async (controller, env, ctx) => {
-    //    switch (controller.cron) {
-    //      case '*/2 * * * *':
-    // Every two minutes
-    await handleCronTrigger(env, ctx);
-    //        break
-    //      case '*/10 * * * *':
-    // Every ten minutes
-    //        await handleRemoteMonitors(env)
-    //       break
-    //    }
+//    switch (controller.cron) {
+//      case '*/2 * * * *':
+        // Every two minutes
+        await handleCronTrigger(env, ctx)
+//        break
+//      case '*/10 * * * *':
+        // Every ten minutes
+//        await handleRemoteMonitors(env)
+ //       break
+//    }
   },
-};
+}
 
-export default handler;
+export default handler
