4c191930f905171de097686d9661dd2b790617bc
Matthew Lew <mdlew@wustl.edu>
2026-01-17 22:33:01 -0600

    Update info icon SVG for improved appearance
    Modified the 'info' icon SVG by adjusting the circle radius and updating the line elements to enhance visual clarity and consistency.
    
    Update info icon SVG to use filled style
    
    Replaces the previous stroked SVG elements for the info icon with new filled path elements and sets fill to 'currentColor'. This change updates the icon's appearance to a filled style for better visual consistency.
    
    Update info icon SVG for improved appearance
    
    Adjusted the stroke width and dimensions of the circles and line in the info icon to enhance visual clarity and consistency.
    
    Update info icon SVG for improved clarity
    
    Replaces the vertical lines in the 'info' icon with a filled circle for the dot and adjusts the line position for better visual representation.

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -33,16 +33,14 @@ const Icon: React.FC<IconProps> = ({ name, className, ...rest }) => {
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth={2}
+          fill='currentColor'
           className={className}
           aria-hidden
           {...rest}
         >
-          <circle cx='12' cy='12' r='9' />
-          <line x1='12' y1='11' x2='12' y2='16' />
-          <circle cx='12' cy='8' r='1.5' />
+          <path d='m12 2.25c-5.382 0-9.75 4.368-9.75 9.75s4.368 9.75 9.75 9.75 9.75-4.368 9.75-9.75-4.368-9.75-9.75-9.75zm0 1.5c4.554 0 8.25 3.696 8.25 8.25s-3.696 8.25-8.25 8.25-8.25-3.696-8.25-8.25 3.696-8.25 8.25-8.25z' />
+          <path d='m8.606 2.735a1.408 1.367 0 1 1 -2.816 0 1.408 1.367 0 1 1 2.816 0z' transform='matrix(1.1989 0 0 1.2342 3.182 4.312)' />
+          <path d='m13.6 16.644c-0.01 0.41 0.182 0.525 0.649 0.573l0.751 0.015v0.768h-5.861v-0.768l0.825-0.015c0.494-0.015 0.612-0.207 0.649-0.573v-4.622c0.005-0.732-0.944-0.617-1.614-0.595v-0.761l4.6-0.166' />
         </svg>
       )
     case 'external-link':
